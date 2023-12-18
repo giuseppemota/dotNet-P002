88bbd7f396a5b0edc544858cb4f9f42a4aeca055
GiuseppeMota <giuseppemota@gmail.com>
2023-12-18 11:24:20 -0300

    Criando projeto

---
+++ Roadmap_ASP_NET_CORE/DevName.cs
@@ -0,0 +1,27 @@
+using System.Text;
+namespace Roadmap_ASP_NET_CORE;
+
+public static class DevName
+{
+    public static string Name => "COLOQUE SEU NOME";
+    public static List<(string, int)> Skills => new List<(string, int)>{
+            ("Fundamentos de C#", 5),
+            ("Habilidades Gerais de Desenvolvimento",4),
+            ("Fundamentos de Banco de Dados",5)
+         };
+    public static string View()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"Nome: {Name}");
+        sb.AppendLine();
+        sb.AppendLine("Habilidades:");
+        foreach (var skill in Skills)
+        {
+            sb.AppendLine($"\t{skill.Item1} - {skill.Item2} estrelas");
+        }
+        var sum = Skills.Sum(x => x.Item2);
+        sb.AppendLine();
+        sb.AppendLine($"Total de estrelas: {sum}");
+        return sb.ToString();
+    }
+}
